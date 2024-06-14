73ac1d2ff41f0269ebd8ab6760a93d220edadd38
Yuliia Tatarinova <yuliia.tatarinova@student.uj.edu.pl>
2024-06-14 22:57:04 +0200

    Fixed model after merges

--- src/NN.py
+++ src/NN.py
@@ -7,99 +7,105 @@ import datatransform
 from jax.scipy.special import logsumexp
 from typing import List, Tuple
 
+
 class Layer:
-    # @jit
+    @jit
     def forward(params, x):
         raise NotImplementedError()
-    
+
     def init_params(rng, shape):
         return jnp.array([])
 
+
 class LayerMatMul(Layer):
-    # @jit
+    @jit
     def forward(params, x):
-      #  print('LayerMatMul:', x.shape, params)
+        #  print('LayerMatMul:', x.shape, params)
         return jnp.dot(x, params)
-    
+
     def init_params(rng, shape):
         input_shape, output_shape = shape
-        W = random.normal(rng, (input_shape, output_shape)) * jnp.sqrt(2.0/input_shape)
+        W = random.normal(rng, (input_shape, output_shape)) * jnp.sqrt(2.0 / input_shape)
         return W
-    
+
+
 class LayerBias(Layer):
-    # @jit
+    @jit
     def forward(params, x):
         # print('LayerBias:', x.shape, params.shape)
-       # print('x:', x)
+        # print('x:', x)
         return x + params
-    
+
     def init_params(rng, shape):
-        return random.normal(rng, shape) * jnp.sqrt(2.0/shape[0])
+        return random.normal(rng, shape) * jnp.sqrt(2.0 / shape[0])
+
 
 class LReLU(Layer):
-    # @jit
+    @jit
     def forward(params, x):
         return jax.nn.leaky_relu(x)
-    
-    
+
+
 class LayerConv2D(Layer):
-    # @jit
+    @jit
     def forward(params, x):
-        #print('LayerConv2D:', x.shape, params.shape)
-       # print('x:', x)
-        out = jax.lax.conv(x, 
+        # print('LayerConv2D:', x.shape, params.shape)
+        # print('x:', x)
+        out = jax.lax.conv(x,
                            jnp.transpose(params, [3, 2, 0, 1]),
                            (1, 1),
                            'SAME')
-        # out = x
         return out
 
     def init_params(rng, shape):
-        return random.normal(rng, shape)* jnp.sqrt(2.0/shape[3])
-    
+        return random.normal(rng, shape) * jnp.sqrt(2.0 / shape[3])
+
+
 class LayerConv2DTranspose(Layer):
     @jit
     def forward(params, x):
-       # print('LayerConv2DTranspose:', x.shape, params)
-       # print('x:', x)
+        # print('LayerConv2DTranspose:', x.shape, params)
+        # print('x:', x)
         params = jnp.transpose(params, [3, 2, 0, 1])
         kernel_rot = jnp.rot90(jnp.rot90(params, axes=(0, 1)), axes=(0, 1))
         # padding = ((2, 1), (2, 1))
-        out = jax.lax.conv(x, 
-                            kernel_rot,
-                            (1, 1),
-                            'SAME')
+        out = jax.lax.conv(x,
+                           kernel_rot,
+                           (1, 1),
+                           'SAME')
         return out
-    
+
     def init_params(rng, shape):
-        return random.normal(rng, shape)* jnp.sqrt(2.0/shape[3])
-    
+        return random.normal(rng, shape) * jnp.sqrt(2.0 / shape[3])
+
+
 class LayerFlatten(Layer):
-    # @jit
+    @jit
     def forward(params, x):
         return jnp.reshape(x, (x.shape[0], -1))
-    
+
+
 class Layer2DReshape(Layer):
     def forward(params, x):
         # make x from vector to square matrix
-        size = x.shape[1] // 16 # TODO: fix later
-        res = jnp.reshape(x, (x.shape[0], 16, int(np.sqrt(size)), int(np.sqrt(size))))
-        res = jnp.transpose(res, [0, 2, 3, 1])
-        return res
-    
+        size = x.shape[1] // 16  # TODO: fix later
+        return jnp.reshape(x, (x.shape[0], 16, int(np.sqrt(size)), int(np.sqrt(size))))
+
     # for some reason gradient is not working if to initialize using init_params and pass the size as param
-    
+
+
 class Layer2DReshape1(Layer):
     def forward(params, x):
         # make x from vector to square matrix
-        size = x.shape[1] // 1 # TODO: fix later
+        size = x.shape[1] // 1  # TODO: fix later
         return jnp.reshape(x, (x.shape[0], 1, int(np.sqrt(size)), int(np.sqrt(size))))
-    
+
+
 class LayerSigmod(Layer):
     @jit
     def forward(params, x):
         print('LayerSigmod:', x.shape)
         return jax.nn.sigmoid(x)
-    
+
     def init_params(rng, shape):
         return random.normal(rng, shape)
--- src/jaxCNN.py
+++ src/jaxCNN.py
@@ -7,18 +7,19 @@ from jax.scipy.special import logsumexp
 
 import NN
 
+
 def one_hot(x, k, dtype=jnp.float32):
     return jnp.array(x[:, None] == jnp.arange(k), dtype)
 
 
 class NeuralNet:
-    def __init__(self, layers, layer_shapes, loss_f): 
+    def __init__(self, layers, layer_shapes, loss_f):
         self.layers = layers
         self.layer_shapes = layer_shapes
         self.loss_f = loss_f
         self.params = []
         self.rng = random.PRNGKey(25)
-        
+
         for i in range(len(layer_shapes)):
             self.params.append(layers[i].init_params(self.rng, layer_shapes[i]))
 
@@ -26,19 +27,18 @@ class NeuralNet:
             for i in range(len(self.layers)):
                 # print(f'Layer {i} - {self.layers[i]}: {params[i].shape}, {x.shape}')
                 x = self.layers[i].forward(params[i], x)
-                # print(f'After Layer {i} - {self.layers[i]}: {x.shape}')
             return x
 
         self.forward = forward
-        
+
         def loss(params, x, y):
             y_hat = forward(params, x)
             return self.loss_f(y_hat, y)
-        
+
         self.loss = loss
-        
+
         print(f"NN initialized with {len(self.params)} layers")
-        
+
     def __call__(self, x):
         return self.forward(self.params, x)
 
@@ -46,16 +46,15 @@ class NeuralNet:
         for i in range(len(self.params)):
             if grads[i].shape == (0,):
                 continue
-            
+
             g_max = jnp.max(jnp.abs(grads[i]))
             self.params[i] = self.params[i] - learning_rate * grads[i]
             # ensure that the weights are not negative
 
-
     def update(self, x, y, learning_rate):
         grads = grad(self.loss)(self.params, x, y)
         self.updateWithGrad(grads, learning_rate)
-        
+
     def train(self, X, y, epochs=100, learning_rate=0.01):
         for _ in range(epochs):
             self.update(X, y, learning_rate)
@@ -63,12 +62,13 @@ class NeuralNet:
             print(f'Epoch: {_}, Loss: {loss}')
 
 
-    
 def MSE(y_hat, y):
-    return jnp.mean((y_hat - y)**2)
-        
+    return jnp.mean((y_hat - y) ** 2)
+
+
 def log_entropy(y_hat, y):
     return -jnp.mean(y_hat * y)
+
 
 def main():
     train_images, train_labels, test_images, test_labels = datatransform.readMNIST('./../data/MNIST/')
@@ -76,11 +76,11 @@ def main():
 
     train_images = jnp.reshape(train_images, (len(train_images), num_pixels))
     test_images = jnp.reshape(test_images, (len(test_images), num_pixels))
-    
+
     num_labels = 10
 
     train_labels = one_hot(train_labels, num_labels)
-    
+
     layers = [
         NN.LayerMatMul,
         NN.LayerBias,
@@ -89,7 +89,7 @@ def main():
         NN.LayerBias,
         NN.LReLU,
     ]
-    
+
     layer_shapes = [
         (784, 200),
         (200,),
@@ -98,23 +98,21 @@ def main():
         (10,),
         (),
     ]
-    
 
-    
     model = NeuralNet(layers, layer_shapes, MSE)
-    
+
     model.train(train_images, train_labels, epochs=100, learning_rate=0.01)
-    
+
     test_images = model(test_images)
     test_labels_hat = jnp.argmax(test_images, axis=1)
-    
+
     accuracy = jnp.mean(test_labels == test_labels_hat)
     print(f'Accuracy: {accuracy}')
-    
+
 
 def simple():
     def fun(x):
-        return x[0]**2 + x[1]**2 + x[2]**2
+        return x[0] ** 2 + x[1] ** 2 + x[2] ** 2
         # return x[0] + 2
 
     layers = [
@@ -126,7 +124,7 @@ def simple():
         NN.LayerBias,
         NN.LReLU,
     ]
-    
+
     layes_shapes = [
         (3, 10),
         (10,),
@@ -135,21 +133,22 @@ def simple():
         (1,),
         (),
     ]
-    
+
     model = NeuralNet(layers, layes_shapes, MSE)
-    
+
     rng = random.PRNGKey(0)
     X = random.normal(rng, (1000, 3))
     y = jnp.array([[fun(x)] for x in X])
-    
+
     # X = jnp.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     # y = jnp.array([[fun(x)] for x in X])
-    
+
     model.train(X, y, epochs=1000, learning_rate=0.05)
-    
+
     n_show = 10
     idx = random.choice(rng, len(X), (n_show,), replace=False)
     print(f"input: \n{X[idx]}, \noutput: \n{model(X[idx])}, \nexpected: \n{y[idx]}")
+
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -166,7 +165,7 @@ class AutoEncoder:
             encoded = self.encoder.forward(encoder_params, x)
             decoded = self.decoder.forward(decoder_params, encoded)
             return decoded
-        
+
         self.forward = forward
 
         def loss(params, x, y):
@@ -176,7 +175,7 @@ class AutoEncoder:
 
             y_hat = self.forward(params, x)
             return self.loss_f(y_hat, y)
-        
+
         self.loss = loss
 
     def __call__(self, x):
@@ -194,15 +193,15 @@ class AutoEncoder:
         self.encoder.updateWithGrad(enc_grads, learning_rate)
         self.decoder.updateWithGrad(dec_grads, learning_rate)
         self.params = (self.encoder.params, self.decoder.params)
-        #print(self.params)
+        # print(self.params)
 
     def train(self, X, y, epochs=100, learning_rate=0.01):
         # implement mini-batch training
         batch_size = 512
         for epoch in range(epochs):
             for i in range(0, len(X), batch_size):
-                x_batch = X[i:i+batch_size]
-                y_batch = y[i:i+batch_size]
+                x_batch = X[i:i + batch_size]
+                y_batch = y[i:i + batch_size]
                 self.update(x_batch, y_batch, learning_rate)
             loss = self.loss(self.params, X, y)
             print(f'Epoch: {epoch}, Loss: {loss}')
@@ -211,32 +210,30 @@ class AutoEncoder:
         # save model into the file
         import pickle
         with open(path, 'wb') as f:
-            pickle.dump((self.params,   self.encoder.layers, self.encoder.forward, self.encoder.loss, \
-                                        self.decoder.layers, self.decoder.forward, self.decoder.loss), f)
+            pickle.dump((self.params, self.encoder.layers, self.encoder.forward, self.encoder.loss, \
+                         self.decoder.layers, self.decoder.forward, self.decoder.loss), f)
 
     def load(self, path):
         import pickle
         with open(path, 'rb') as f:
             self.params, self.encoder.layers, self.encoder.forward, self.encoder.loss, \
-             self.decoder.layers, self.decoder.forward, self.decoder.loss = pickle.load(f)
-
-
+                self.decoder.layers, self.decoder.forward, self.decoder.loss = pickle.load(f)
 
 
 # not done yet
 def conv_test():
     size = 28
-    
+
     rng = random.PRNGKey(0)
     X = random.normal(rng, (1000, size, size))
     X = X.reshape((1000, size, size, 1))
     X = X.transpose((0, 3, 1, 2))
-    
+
     def fun(x):
         return jnp.sum(jnp.abs(x))
-    
+
     y = jnp.array([[fun(x)] for x in X])
-    
+
     layers = [
         NN.LayerConv2D,
         NN.LReLU,
@@ -247,7 +244,7 @@ def conv_test():
         NN.LayerBias,
         NN.LReLU,
     ]
-    
+
     n_channels = 3
     layer_shapes = [
         (3, 3, 1, 1),
@@ -255,15 +252,16 @@ def conv_test():
         (3, 3, 1, 1),
         (),
         (),
-        (size*size, 1),
+        (size * size, 1),
         (1,),
         (),
     ]
-    
+
     model = NN.NeuralNet(layers, layer_shapes, MSE)
-    
+
     model.train(X, y, epochs=1000, learning_rate=0.01)
     # model.forward(model.params, X)
+
 
 def autoencoder_test():
     # print current working directory
@@ -271,9 +269,9 @@ def autoencoder_test():
     training_labels_filepath = '../data/MNIST/train-labels-idx1-ubyte/train-labels-idx1-ubyte'
     test_images_filepath = '../data/MNIST/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte'
     test_labels_filepath = '../data/MNIST/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte'
-    
+
     mnist_dataloader = datatransform.MnistDataloader(training_images_filepath, training_labels_filepath,
-                                       test_images_filepath, test_labels_filepath)
+                                                     test_images_filepath, test_labels_filepath)
     (train_images, train_labels), (test_images, test_labels) = mnist_dataloader.load_data()
     train_images = np.array(train_images)
     test_images = np.array(test_images)
@@ -306,14 +304,14 @@ def autoencoder_test():
         NN.LayerBias,
         NN.LReLU,
     ]
-    
+
     encoder_layer_shapes = [
-        (3,3,1,8),
+        (3, 3, 1, 8),
         (),
-        (3,3,8,16),
+        (3, 3, 8, 16),
         (),
         (),
-        (28*28*16, 32),
+        (28 * 28 * 16, 32),
         (32,),
         (),
         (32, 16),
@@ -322,21 +320,21 @@ def autoencoder_test():
     ]
 
     decoder_layers = [
-       NN.LayerMatMul,
-       NN.LayerBias,
-       NN.LReLU,
-       NN.Layer2DReshape,
-       NN.LayerConv2DTranspose,
-       NN.LReLU,
-       NN.LayerConv2DTranspose,
-       NN.LReLU,
-       NN.LayerConv2DTranspose,
-       NN.LayerSigmod,
+        NN.LayerMatMul,
+        NN.LayerBias,
+        NN.LReLU,
+        NN.Layer2DReshape,
+        NN.LayerConv2DTranspose,
+        NN.LReLU,
+        NN.LayerConv2DTranspose,
+        NN.LReLU,
+        NN.LayerConv2DTranspose,
+        NN.LayerSigmod,
     ]
 
     decoder_layer_shapes = [
-        (16, 16*28*28),
-        (16*28*28,),
+        (16, 16 * 28 * 28),
+        (16 * 28 * 28,),
         (),
         (),
         (3, 3, 16, 8),
@@ -345,9 +343,8 @@ def autoencoder_test():
         (),
         (3, 3, 4, 1),
         (),
-        
-    ]
 
+    ]
 
     model = AutoEncoder(encoder_layers, encoder_layer_shapes, decoder_layers, decoder_layer_shapes, MSE)
 
@@ -361,7 +358,7 @@ def autoencoder_test():
     test_images = test_images.astype(jnp.float32)
     train_images = train_images / 255
     test_images = test_images / 255
-    
+
     print('Shape: ', train_images.shape)
 
     model.train(train_images, train_images, epochs=5, learning_rate=0.1)
@@ -378,12 +375,10 @@ def autoencoder_test():
     datatransform.save_images(test_images, '../output')
 
 
-
 if __name__ == "__main__":
     # jax.config.update("jax_traceback_filtering", "off")
     # main()
     # simple()
     # conv_test()
     autoencoder_test()
-    
-    
+
